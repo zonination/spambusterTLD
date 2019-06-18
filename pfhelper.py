55f9ceb6eaaf4bd71bd6b7e95a438f03833eb905
Zoni Nation <zonination@gmail.com>
2019-06-18 11:48:48 -0400

    Initial commit

---
+++ pfhelper.py
@@ -0,0 +1,44 @@
+import praw
+import time
+import rlogin
+import prawcore
+
+# Log in
+r = rlogin.pf()
+print('Logged in as: {0}'.format(r.user.me()))
+print('')
+
+# List our TLD blacklist
+blacklist = ['stocktradings.market']
+
+while True:
+    try:
+        for item in r.subreddit('mod').mod.modqueue(only='comments'):
+            for black in blacklist:
+                if black in str.split(item.body, '/'):
+                    item.mod.remove(spam=True)
+                    r.subreddit(item.subreddit.display_name).banned.add(item.author.name, ban_reason='spam: {0}'.format(black), ban_message='spam: {0}'.format(black))
+                    print('Banned /u/{0} from /r/{1} for spamming {2}'.format(item.author.name, item.subreddit.display_name, black))
+                    
+        time.sleep(5)
+    # Exception list for when Reddit inevitably screws up
+    except praw.exceptions.APIException:
+        print('\nAn API exception happened.\nTaking a coffee break.\n')
+        time.sleep(30)
+    except prawcore.exceptions.ServerError:
+        print('\nReddit\'s famous 503 error occurred.\nTaking a coffee break.\n')
+        time.sleep(180)
+    except prawcore.exceptions.InvalidToken:
+        print('\n401 error: Token needs refreshing.\nTaking a coffee break.\n')
+        time.sleep(30)
+    # Probably another goddamn Snoosletter that the bot can't reply to.
+    except prawcore.exceptions.Forbidden:
+        print('  Unable to respond. Marking as read.\n')
+        for item in r.inbox.unread(limit=100):
+            if item in r.inbox.messages(limit=100):
+                item.mark_read()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        print('\nException happened (PFhelper).\nTaking a coffee break.\n')
+        time.sleep(30)
