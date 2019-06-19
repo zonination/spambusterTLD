59c311002d1e19e98e680ec7111585d36a3ceb34
Zoni Nation <zonination@gmail.com>
2019-06-19 09:48:42 -0400

    Add files via upload

--- pfhelper2.py
+++ pfhelper2.py
@@ -14,6 +14,7 @@ blacklist = ['stockprice.com', 'truetradinggroup.com']
 
 while True:
     try:
+        # MODULE FOR COMMENTS
         for item in r.subreddit('mod').mod.modqueue(only='comments'):
             for string in str.split(item.body, '('):
                 if string.startswith('http'):
@@ -23,8 +24,18 @@ while True:
                         item.mod.remove(spam=True)
                         r.subreddit(item.subreddit.display_name).banned.add(item.author.name, ban_reason='spam: {0}'.format(justice), ban_message='spam: {0}'.format(justice))
                         print('Banned /u/{0} from /r/{1} for spamming {2}'.format(item.author.name, item.subreddit.display_name, justice))
+        # MODULE FOR SUBMISSIONS
+        for item in r.subreddit('mod').mod.modqueue(only='submissions'):
+            for string in str.split(item.selftext, '('):
+                if string.startswith('http'):
+                    justice = str.split(requests.get(str.split(string, ')')[0]).url, '/')[2]
+                    # If you want an age filter, add: and ((time.time() - item.author.created_utc) < (60 * 60 * 24 * 30))
+                    if (justice in blacklist):
+                        item.mod.remove(spam=True)
+                        r.subreddit(item.subreddit.display_name).banned.add(item.author.name, ban_reason='spam: {0}'.format(justice), ban_message='spam: {0}'.format(justice))
+                        print('Banned /u/{0} from /r/{1} for spamming {2}'.format(item.author.name, item.subreddit.display_name, justice))
                     
-        time.sleep(120)
+        time.sleep(30)
     # Exception list for when Reddit inevitably screws up
     except praw.exceptions.APIException:
         print('\nAn API exception happened.\nTaking a coffee break.\n')
@@ -43,6 +54,6 @@ while True:
                 item.mark_read()
     except (KeyboardInterrupt, SystemExit):
         raise
-    except:
-        print('\nException happened (PFhelper).\nTaking a coffee break.\n')
-        time.sleep(30)
+#    except:
+#        print('\nException happened (PFhelper).\nTaking a coffee break.\n')
+#        time.sleep(30)
