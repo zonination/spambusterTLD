76d504afdae5ea87073d04288d2f10ff29b1096c
Zoni Nation <zonination@gmail.com>
2019-07-12 11:38:59 -0400

    Add files via upload

--- pfhelper3.py
+++ pfhelper3.py
@@ -9,7 +9,7 @@ print('Logged in as: {0}'.format(r.user.me()))
 print('')
 
 # Blacklist
-blist = ['viajarencamboya.com', 'hangbiz.com', 'httpexploracamboya.com']
+blist = ['viajarencamboya.com', 'hangbiz.com', 'httpexploracamboya.com', 'freemom.httpexploracamboya.com', 'thekhmerfood.com']
 
 # MODULE FOR SUBMISSIONS
 def submissions():
@@ -24,8 +24,12 @@ def submissions():
             # King's Justice has been delivered. Now show proof and log the file.
             print('Banned /u/{0} from /r/{1} for spamming {2}'.format(item.author.name, item.subreddit.display_name, item.domain))
             f = open('log.csv', 'a+')
-            f.write('{0},{1},{2},https://www.reddit.com{3}\n'.format(item.author.name, item.subreddit.display_name, item.domain, item.permalink))
+            f.write('{4},{0},{1},{2},https://www.reddit.com{3}\n'.format(item.author.name, item.subreddit.display_name, item.domain, item.permalink, item.created))
             f.close()
+            
+            # Write a helpdesk ticket
+            r.subreddit('reddit.com').message('Help Center Report','\nNew Help Center report has been received.\n\nReport details:\n- Report Reason: This is spam\n\n- Reported Users\n1) {0} (https://www.reddit.com/u/{0})\n\n\n\n\n        Custom Text: Spam from domain {3} made at {1} (GTC epoch), posted to https://www.reddit.com{2}\n\n'.format(item.author.name, item.created, item.permalink, item.domain))
+            print(' + reported /u/{0} to the admins.\n'.format(item.author.name))
             return
 
 
@@ -55,6 +59,6 @@ while True:
                 item.mark_read()
     except (KeyboardInterrupt, SystemExit):
         raise
-    except:
-        print('\nException happened (PFhelper).\nTaking a coffee break.\n')
-        time.sleep(30)
+#    except:
+#        print('\nException happened (PFhelper).\nTaking a coffee break.\n')
+#    time.sleep(30)
